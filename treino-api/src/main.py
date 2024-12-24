6281becd316fb11a75e66fbf8494a2c98fd41c89
h3nrey <pedro.novaes.education@gmail.com>
2024-12-24 16:31:22 -0300

    feat: add base route for api

--- treino-api/src/main.py
+++ treino-api/src/main.py
@@ -3,4 +3,8 @@ from routers.exercise import router as exercise_router
 
 app = FastAPI()
 
+@app.get("/")
+def base_route():
+    return "Treino legal API 💪"
+
 app.include_router(exercise_router, prefix="/exercises", tags=["exercises"])
