a1294269d3f795d349f420d7728e27754e43a040
h3nrey <pedro.novaes.education@gmail.com>
2025-01-05 10:04:12 -0300

    feat: setting basic cors config

--- treino-api/src/main.py
+++ treino-api/src/main.py
@@ -1,10 +1,28 @@
 from fastapi import FastAPI
 from routers.exercise import router as exercise_router
-
+from routers.seeders import run_seeds
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+origins = [
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def base_route():
     return "Treino legal API 💪"
+
+@app.get("/seeders")
+async def run_seeders():
+    result = await run_seeds()
+    return result
 
 app.include_router(exercise_router, prefix="/exercises", tags=["exercises"])
