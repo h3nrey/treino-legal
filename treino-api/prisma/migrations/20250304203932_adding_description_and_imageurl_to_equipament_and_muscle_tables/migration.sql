6a071bf3c998bed45f3de9de622cb17b6c897a3a
h3nrey <pedro.novaes.education@gmail.com>
2025-03-04 17:42:37 -0300

    FEAT: adding description and imageUrl to muscle and equipaments tables

---
+++ treino-api/prisma/migrations/20250304203932_adding_description_and_imageurl_to_equipament_and_muscle_tables/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Equipament" ADD COLUMN     "description" TEXT,
+ADD COLUMN     "imageUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "Muscle" ADD COLUMN     "description" TEXT,
+ADD COLUMN     "imageUrl" TEXT;
