97f9a8057ba5ee53cc27322ca18b0d3cd775b54f
h3nrey <pedro.novaes.education@gmail.com>
2025-04-12 18:12:44 -0300

    feat: adicionando as rotas para favoritar e desfavoritar um exercicio

--- treino-api/src/controllers/user.controller.ts
+++ treino-api/src/controllers/user.controller.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { AuthRequest } from "../middleware/auth";
+import { connect } from "http2";
 
 const prisma = new PrismaClient();
 
@@ -82,6 +83,85 @@ export async function loginUser(req: Request, res: Response) {
 
 export function getMe(req: AuthRequest, res: Response) {
   res.json(req.user);
+}
+
+export function getFavoritedExercises(req: AuthRequest, res: Response) {
+  const { username } = req.user as { username: string };
+
+  prisma.user
+    .findUnique({
+      where: {
+        username,
+      },
+      include: {
+        UserExercises: {
+          include: {
+            exercise: true,
+          },
+        },
+      },
+    })
+    .then((user) => {
+      if (!user) {
+        res.status(404).json({ message: "User not found" });
+        return;
+      }
+
+      res.json(user.UserExercises);
+    })
+    .catch((error) => {
+      console.error("Error fetching user:", error);
+      res.status(500).json({ message: "Internal server error" });
+    });
+}
+
+export function favoriteExercise(req: AuthRequest, res: Response) {
+  const { username } = req.user as { username: string };
+  const exerciseId = req.params.exerciseId;
+
+  if (!exerciseId) {
+    res.status(400).json({ message: "Exercise ID is required" });
+    return;
+  }
+
+  prisma.userExercises
+    .create({
+      data: {
+        user: { connect: { username } },
+        exercise: { connect: { id: parseInt(exerciseId) } },
+      },
+    })
+    .then(() => {
+      res.status(201).json({ message: "Exercise favorited" });
+    })
+    .catch((error) => {
+      console.error("Error favoriting exercise:", error);
+      res.status(500).json({ message: "Internal server error" });
+    });
+}
+
+export async function unFavoriteExercise(req: AuthRequest, res: Response) {
+  const { username } = req.user as { username: string };
+  const exerciseId = req.params.exerciseId;
+
+  if (!exerciseId) {
+    res.status(400).json({ message: "Exercise ID is required" });
+  }
+
+  prisma.userExercises
+    .deleteMany({
+      where: {
+        user: { username },
+        exerciseId: parseInt(exerciseId),
+      },
+    })
+    .then(() => {
+      res.status(200).json({ message: "Exercise unfavorited" });
+    })
+    .catch((error) => {
+      console.error("Error unfavoriting exercise:", error);
+      res.status(500).json({ message: "Internal server error" });
+    });
 }
 
 function generateToken(username: string) {
--- treino-api/src/routes/user.routes.ts
+++ treino-api/src/routes/user.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import { createUser, getMe, loginUser } from "../controllers/user.controller";
+import {
+  createUser,
+  favoriteExercise,
+  getFavoritedExercises,
+  getMe,
+  loginUser,
+  unFavoriteExercise,
+} from "../controllers/user.controller";
 import { authMiddleware } from "../middleware/auth";
 const router = Router();
 
-router.get("/:id", authMiddleware, getMe);
+router.get("/me", authMiddleware, getMe);
 router.post("/signup", createUser);
 router.post("/login", loginUser);
+router.get("/me/favorites", authMiddleware, getFavoritedExercises);
+router.post("/me/favorites/:exerciseId", authMiddleware, favoriteExercise);
+router.delete("/me/favorites/:exerciseId", authMiddleware, unFavoriteExercise);
 
 export default router;
