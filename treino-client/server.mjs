1ff59ae5d8dbbd35f35694fb75473b9c26f938c3
h3nrey <pedro.novaes.education@gmail.com>
2025-04-29 16:21:14 -0300

    feat: mudando configurações do angular.json

--- treino-client/server.mjs
+++
@@ -1,8 +0,0 @@
-import { createServer } from 'node:http';
-import { app } from './dist/project-name/server/main.js';
-
-const port = parseInt(process.env.PORT || '4200', 10);
-
-createServer(app).listen(port, () => {
-  console.log(`Node server listening on http://localhost:${port}`);
-});
