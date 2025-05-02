fd81e4e83e67dd114e8be690ff7325c5a05dbf71
h3nrey <pedro.novaes.education@gmail.com>
2025-05-01 22:42:06 -0300

    feat: mudando a api url

--- treino-client/src/enviroments/enviroment.prod.ts
+++ treino-client/src/enviroments/enviroment.prod.ts
@@ -2,5 +2,5 @@ import { Enviroment } from "../app/utils/interfaces";
 
 export const environment: Enviroment = {
     production: true,
-    apiUrl: 'http://44.206.235.66:3000',
+    apiUrl: 'https://fichatreinoapi.duckdns.org',
 }
