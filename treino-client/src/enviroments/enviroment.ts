bb0cbab931e45c1d91bfce0b194fdc773b19ce87
h3nrey <pedro.novaes.education@gmail.com>
2025-08-21 08:08:32 -0300

    changes

--- treino-client/src/enviroments/enviroment.ts
+++ treino-client/src/enviroments/enviroment.ts
@@ -1,4 +1,4 @@
 import { environment as enviromentDev } from "./enviroment.dev";
 import { environment as enviromentProd } from "./enviroment.prod";
 
-export const environment = enviromentProd;
+export const environment = enviromentDev;
