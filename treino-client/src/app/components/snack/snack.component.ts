1201d141daa7498e9c277f8e8fee23e4fe1d5522
h3nrey <pedro.novaes.education@gmail.com>
2025-08-23 13:19:52 -0300

    feat: modificando o estilo do component de snack

--- treino-client/src/app/components/snack/snack.component.ts
+++ treino-client/src/app/components/snack/snack.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
 import { SnackMessage, SnackService } from '../../services/snack.service';
 import { CommonModule } from '@angular/common';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-snack',
   imports: [CommonModule],
   templateUrl: './snack.component.html',
-  styleUrl: './snack.component.scss'
+  styleUrl: './snack.component.scss',
+  animations: [
+    trigger('snackAnimation', [
+      transition(':enter', [
+        style({ 
+          opacity: 0, 
+          transform: 'translateX(-50%)' 
+        }),
+        animate('300ms ease-out', style({ 
+          opacity: 1, 
+          transform: 'translateX(0)' 
+        })),
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ 
+          opacity: 0, 
+          transform: 'translateX(50%)' 
+        })),
+      ]),
+    ]),
+  ]
 })
 export class SnackComponent {
   snack: SnackMessage | null = null;
